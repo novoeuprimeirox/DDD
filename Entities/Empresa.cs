5897cb1bfe17df0571dfe1c9542baa3664a59f54
Alex Nunes Feitosa <anfmidway@gmail.com>
2022-03-16 17:52:07 -0300

    Criando Entitie Empresa

--- Entities/Empresa.cs
+++ Entities/Empresa.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities
 {
+    [Table("Empresa")]
     public class Empresa
     {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O nome da empresa é obrigatório", AllowEmptyStrings = false)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O site é obrigatório", AllowEmptyStrings = false)]
+        public string Site { get; set; }
+
+        [Display(Name = "Quantida de funcionários")]
+        public int QuantidadeFuncionarios { get; set; }
     }
 }
