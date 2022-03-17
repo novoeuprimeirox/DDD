c94992bd5fdb8c0c5804d0628d64b006dbae219e
Alex Nunes Feitosa <anfmidway@gmail.com>
2022-03-16 21:38:45 -0300

    ContextBase

--- Entities/Notifications/Notifies.cs
+++ Entities/Notifications/Notifies.cs
@@ -76,4 +76,4 @@ namespace Entities
 
     }
 }
-}
+ 
--- Infrastructure/Configuration/ContextBase.cs
+++ Infrastructure/Configuration/ContextBase.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Entities;
 
 namespace Infrastructure.Configuration
-{
-    public class ContextBase
+{ 
+    public class ContextBase : DbContext
     {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+        public DbSet<Empresa> Compras { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Empresa>().ToTable("Empresa").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=anfsusax;Initial Catalog=MVCTeste;Integrated Security=True";
+            // string strCon = "Server=RI-DEV-03\SQLEXPRESS;Database=MVCTeste;User Id=candidato;Password=Teste123;" providerName="System.Data.SqlClient"/>";
+            return strCon;
+        }
     }
 }
