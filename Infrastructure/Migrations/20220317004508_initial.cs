70805ca128acee1579db32b889879ffcce8ba1ac
Alex Nunes Feitosa <anfmidway@gmail.com>
2022-03-16 21:45:49 -0300

    Criando Migration

---
+++ Infrastructure/Migrations/20220317004508_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Site = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuantidadeFuncionarios = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empresa");
+        }
+    }
+}
--- WebEmpresaX/Startup.cs
+++ WebEmpresaX/Startup.cs
@@ -1,6 +1,8 @@
+using Infrastructure.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,13 +25,11 @@ namespace WebEmpresaX
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //        services.AddDbContext<ContextBase>(options =>
-            //options.UseSqlServer(
-            //    Configuration.GetConnectionString("DefaultConnection")));
-            //        services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //            .AddEntityFrameworkStores<ContextBase>();
-            //        services.AddControllersWithViews();
-            //        services.AddRazorPages();
+            services.AddDbContext<ContextBase>(options =>
+    options.UseSqlServer(
+        Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddRazorPages();
             //        HelpStartup.ConfigureSingleton(services);
 
             services.AddControllersWithViews();
